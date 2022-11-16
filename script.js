3ffac36a786e799f004ce08c6c554f3267122eb4
PaoloNapoli97 <paolonapoli@live.it>
2022-11-16 15:06:07 +0100

    Add: vuejs Api Script

---
+++ script.js
@@ -0,0 +1,17 @@
+const { createApp } = Vue;
+
+createApp({
+    data(){
+        return{
+            mails: []
+        }
+    },
+    created() {
+        for ( let i = 0; i < 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) => {
+                this.mails.push(response.data.response);
+            });
+        }
+    }
+}).mount('#app');
